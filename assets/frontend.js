2c3718084370476cd24f8715506bee4b45534d28
Sahand Rezvan <sahand.rezvanazar@gmail.com>
2026-09-20 09:31:11 +0330

    Remove legacy 1.1 frontend assets

--- assets/frontend.js
+++
@@ -1,7 +0,0 @@
-(function($){
-  function refreshDates(){
-    if(!window.SDFW) return;
-    $.post(SDFW.ajax,{action:'sdfw_dates',nonce:SDFW.nonce}).always(function(){});
-  }
-  $(document.body).on('updated_checkout', refreshDates);
-})(jQuery);
